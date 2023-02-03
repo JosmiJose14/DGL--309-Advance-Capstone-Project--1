943e3321ff826806f9ceb4eb67e161a7758fe2b3
Josmi Jose <josmijose100@gmail.com>
2023-02-02 23:20:57 -0800

    Header added

--- design-systems/src/App.js
+++ design-systems/src/App.js
@@ -6,26 +6,11 @@ import React from 'react';
 export default App;
 function App() {
   return (
-      <div>
-          <nav class="navbar background">
-              <ul class="nav-list">                
-                  <div class="logo">
-                      <img src=
-"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png" />
-                  </div>
-                  <li><a href="#courses">Courses</a></li>
-                  <li><a href="#tutorials">Tutorials</a></li>
-                  <li><a href="#jobs">Jobs</a></li>
-                  <li><a href='#student'>Student</a></li>
-              </ul>
-          </nav>
-
-         
-          <footer className="footer">
-              <p className="text-footer">
-                  Copyright ©-All rights are reserved
-              </p>
-          </footer>
+        <div id="designSystem">
+        <div id="divHeading"> 
+           <h1>HAIR WITH FLAIR SALON</h1>
+           <h3>UNISEX SALON</h3></div>
+    
       </div>
   )
 }
