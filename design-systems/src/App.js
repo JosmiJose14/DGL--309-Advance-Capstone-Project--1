388c7a144ae0c40810e3d37f1df75e329f82113a
Josmi Jose <josmijose100@gmail.com>
2023-02-05 11:06:17 -0800

    navigation bar html and css

--- design-systems/src/App.js
+++ design-systems/src/App.js
@@ -9,8 +9,25 @@ function App() {
         <div id="designSystem">
         <div id="divHeading"> 
            <h1>HAIR WITH FLAIR SALON</h1>
-           <h3>UNISEX SALON</h3></div>
-    
+           <h3>UNISEX SALON</h3></div>    
+        
+        <nav className="navigation">
+      
+        <div
+          className="navigation-menu">
+          <ul>
+            <li>
+              <a href="/home">Typography & Color</a>
+            </li>
+            <li>
+              <a href="/about">Forms</a>
+            </li>
+            <li>
+              <a href="/contact">Components</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       </div>
   )
 }
