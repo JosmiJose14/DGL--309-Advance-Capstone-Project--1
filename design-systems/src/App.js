ffaab0d464871ff27f6899bba2c4202b28395220
Josmi Jose <josmijose100@gmail.com>
2023-02-05 13:30:02 -0800

    Typography basic detailed layout without values

--- design-systems/src/App.js
+++ design-systems/src/App.js
@@ -9,10 +9,8 @@ function App() {
         <div id="designSystem">
         <div id="divHeading"> 
            <h1>HAIR WITH FLAIR SALON</h1>
-           <h3>UNISEX SALON</h3></div>    
-        
-        <nav className="navigation">
-      
+           <h3>UNISEX SALON</h3></div> 
+        <nav className="navigation">      
         <div
           className="navigation-menu">
           <ul>
@@ -28,6 +26,45 @@ function App() {
           </ul>
         </div>
       </nav>
+      <div id="bdyDesignSystems">
+        <div id="typography">
+          <h3>Typography & Colors</h3>
+          <div id="typography">
+            <h4>Fonts</h4>
+            <span> font-family: Josefin Sans</span>
+            <span> font-size: 16px</span>
+            <h4>Headers</h4>
+            <span> H1 - Logo </span><br/>
+            <span> H2  - sub headings</span><br/>
+            <span> H3  -Logo sub heading</span><br/>
+            <span> H4 </span><br/>
+            <span> h4 -image heading</span><br/>
+            <span> h4 </span> <br/>
+            <h4>Colors</h4> 
+            <span> body text : #color </span> <br/>  
+            <span> titles or display text : #color</span> <br/>
+            <span>background : #color</span> <br/>
+            <span>alternate background : #color</span> <br/>
+            <span>primary for buttons: #color</span> <br/>
+            <span>secondary colous for buttons: #color</span> <br/>
+            <span>success button or message: #color</span> <br/>
+            <span> danger button or message: #color</span> <br/>
+            <span> info button or message: #color</span> <br/>
+            <span> warning button or message: #color</span> <br/> 
+            <h4>Spacing</h4> 
+            <span> body margin : #value </span> <br/>
+            <span> body padding : #value </span><br/>
+            <span> div margin : #value </span> <br/>
+            <span> div padding : #value </span><br/>
+            <h4>Images </h4> 
+            <span> image width : #value </span><br/>
+            <span> image height : #value </span><br/>
+            <span> image border style : #value </span><br/>
+            <span> image styles: #value </span><br/>
+          </div>
+
+        </div>
+      </div>
       </div>
   )
 }
