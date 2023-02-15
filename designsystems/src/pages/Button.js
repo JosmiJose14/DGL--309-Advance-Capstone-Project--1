5e8743a76e5cf4220ea38e185ff677fea4075be5
Josmi Jose <josmijose100@gmail.com>
2023-02-15 11:41:45 -0800

    style correction

--- designsystems/src/pages/Button.js
+++ designsystems/src/pages/Button.js
@@ -8,15 +8,15 @@ export const ButtonStyles = () => {
       <h2>Buttons</h2>
       <p>Buttons in web development refer to clickable elements that perform a specific action when clicked.</p>
       <div class="flex-container">
-        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff',height: '580px',  overflow: 'scroll' }}>
-          <div className="card-body"  style={{height: '580px'}}>
+        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff', height: '580px', overflow: 'scroll' }}>
+          <div className="card-body" style={{ height: '580px' }}>
             <p className="card-title"><h4>Submit and Reset Button</h4>A submit button is used to submit a form and is typically labeled "Submit". When clicked, it sends the data entered in the form to a server for processing.</p>
             <p>A reset button is used to reset all the form fields to their default values. It is usually labeled "Reset".</p>
             <p className="card-text"> {
-              <div style={{ textAlign: 'center' }}> <input type="submit" value="Submit" id="buttons" /><input type="reset" value="Reset" id="buttons" style={{marginTop:'1%'}}/></div>
+              <div style={{ textAlign: 'center' }}> <input type="submit" value="Submit" id="buttons" /><input type="reset" value="Reset" id="buttons" style={{ marginTop: '1%' }} /></div>
             /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
               <div class="expandHideCode" >html
-              <br></br><br></br>
+                <br></br><br></br>
               </div>
               {isExpanded && (
                 <pre>
@@ -29,7 +29,7 @@ export const ButtonStyles = () => {
             <p className="card-text"> {
             /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
               <div class="expandHideCode">css
-              <br></br><br></br>               
+                <br></br><br></br>
               </div>
               {isExpanded && (
                 <pre>
@@ -68,14 +68,14 @@ export const ButtonStyles = () => {
               )}</p>
           </div>
         </div>
-        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff',height: '580px',  overflow: 'scroll' }}>
-          <div className="card-body" style={{height:'580px'}}>
-            <p className="card-title"><h4>Inactive Button</h4>Disabled buttons are buttons that are non-interactive and do not respond to user actions. They are typically grayed out and may display a message indicating that they are disabled. This can be useful in certain scenarios where you want to prevent users from clicking a button until certain conditions are met.</p><br/>
+        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff', height: '580px', overflow: 'scroll' }}>
+          <div className="card-body" style={{ height: '580px' }}>
+            <p className="card-title"><h4>Inactive Button</h4>Disabled buttons are buttons that are non-interactive and do not respond to user actions. They are typically grayed out and may display a message indicating that they are disabled. This can be useful in certain scenarios where you want to prevent users from clicking a button until certain conditions are met.</p><br />
             <p className="card-text"> {
-              <div style={{ textAlign: 'center' }}> <input type="reset" value="Inactive" id="buttons" disabled style={{backgroundColor:'#5c494f'}}/></div>
+              <div style={{ textAlign: 'center' }}> <input type="reset" value="Inactive" id="buttons" disabled style={{ backgroundColor: '#5c494f' }} /></div>
             /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
               <div class="expandHideCode" >html
-              <br></br><br></br>
+                <br></br><br></br>
               </div>
               {isExpanded && (
                 <pre>
@@ -87,7 +87,7 @@ export const ButtonStyles = () => {
             <p className="card-text"> {
             /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
               <div class="expandHideCode">css
-              <br></br><br></br>
+                <br></br><br></br>
               </div>
               {isExpanded && (
                 <pre>
--- designsystems/src/pages/StyleElements.js
+++ designsystems/src/pages/StyleElements.js
@@ -15,132 +15,132 @@ export const Typography = () => {
         developers to create visually appealing and effective headings.
       </p>
       <br />
-      <div style={{height: '650px', overflow: 'auto'}}>
-      <table>
-        <tbody>
-          <tr>
-            <td>Heading one</td>
-            <td>
-              <h1>This is h1 tag.</h1>
-            </td>
-            <td>
-              <pre>
-                {`<h1>This is h1 tag.</h1>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Heading two</td>
-            <td>
-              <h2>This is h2 tag.</h2>
-            </td>
-            <td>
-              <pre>
-                {`<h2>This is h2 tag.</h2>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Heading three</td>
-            <td>
-              <h3>This is h3 tag.</h3>
-            </td>
-            <td>
-              <pre>
-                {`<h3>This is h3 tag.</h3>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Heading four</td>
-            <td>
-              <h4>This is h4 tag.</h4>
-            </td>
-            <td>
-              <pre>
-                {`<h4>This is h4 tag.</h4>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Heading five</td>
-            <td>
-              <h5>This is h5 tag.</h5>
-            </td>
-            <td>
-              <pre>
-                {`<h5>This is h5 tag.</h5>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Heading six</td>
-            <td>
-              <h6>This is h6 tag.</h6>
-            </td>
-            <td>
-              <pre>
-                {`<h6>This is h6 tag.</h6>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>p tag </td>
-            <td>
-              <p>This is p tag.</p>
-            </td>
-            <td>
-              <pre>
-                {`<p>This is p tag.</h6>`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Font-Family for headings - Josefin-Sans</td>
-            <td>
-              <h3 style={{ fontFamily: 'Josefin Sans' }}>This is Josefin-Sans tag.</h3>
-            </td>
-            <td>
-              <pre>
-                {`{font-family: 'Josefin Sans';}`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Font-Family for body text - sans-serif</td>
-            <td>
-              <h3 style={{ fontFamily: 'sans-serif' }}>This is sans-serif font.</h3>
-            </td>
-            <td>
-              <pre>
-                {`{fontFamily: 'sans-serif';}`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Font-size for body text - 16px</td>
-            <td>
-              <p style={{ fontSize: '16px' }}>This is 16px.</p>
-            </td>
-            <td>
-              <pre>
-                {`{font-size: '16px';}`}
-              </pre>
-            </td>
-          </tr>
-          <tr>
-            <td>Font-size for image caption - 16px</td>
-            <td>
-              <p style={{ fontSize: '16px' }}>This is 16px.</p>
-            </td>
-            <td>
-              <pre>
-                {`{font-size: '16px';}`}
-              </pre>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div style={{ height: '650px', overflow: 'auto' }}>
+        <table>
+          <tbody>
+            <tr>
+              <td>Heading one</td>
+              <td>
+                <h1>This is h1 tag.</h1>
+              </td>
+              <td>
+                <pre>
+                  {`<h1>This is h1 tag.</h1>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Heading two</td>
+              <td>
+                <h2>This is h2 tag.</h2>
+              </td>
+              <td>
+                <pre>
+                  {`<h2>This is h2 tag.</h2>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Heading three</td>
+              <td>
+                <h3>This is h3 tag.</h3>
+              </td>
+              <td>
+                <pre>
+                  {`<h3>This is h3 tag.</h3>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Heading four</td>
+              <td>
+                <h4>This is h4 tag.</h4>
+              </td>
+              <td>
+                <pre>
+                  {`<h4>This is h4 tag.</h4>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Heading five</td>
+              <td>
+                <h5>This is h5 tag.</h5>
+              </td>
+              <td>
+                <pre>
+                  {`<h5>This is h5 tag.</h5>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Heading six</td>
+              <td>
+                <h6>This is h6 tag.</h6>
+              </td>
+              <td>
+                <pre>
+                  {`<h6>This is h6 tag.</h6>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>p tag </td>
+              <td>
+                <p>This is p tag.</p>
+              </td>
+              <td>
+                <pre>
+                  {`<p>This is p tag.</h6>`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Font-Family for headings - Josefin-Sans</td>
+              <td>
+                <h3 style={{ fontFamily: 'Josefin Sans' }}>This is Josefin-Sans tag.</h3>
+              </td>
+              <td>
+                <pre>
+                  {`{font-family: 'Josefin Sans';}`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Font-Family for body text - sans-serif</td>
+              <td>
+                <h3 style={{ fontFamily: 'sans-serif' }}>This is sans-serif font.</h3>
+              </td>
+              <td>
+                <pre>
+                  {`{fontFamily: 'sans-serif';}`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Font-size for body text - 16px</td>
+              <td>
+                <p style={{ fontSize: '16px' }}>This is 16px.</p>
+              </td>
+              <td>
+                <pre>
+                  {`{font-size: '16px';}`}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>Font-size for image caption - 16px</td>
+              <td>
+                <p style={{ fontSize: '16px' }}>This is 16px.</p>
+              </td>
+              <td>
+                <pre>
+                  {`{font-size: '16px';}`}
+                </pre>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
@@ -148,7 +148,7 @@ export const Typography = () => {
 
 export const Colors = () => {
   return (
-    <div  class="topPadding clsColors marginleft">
+    <div class="topPadding clsColors marginleft">
       <h2>Colors</h2>
       <p>In a design system, different colors are used to
         create visual hierarchy, convey meaning, and establish
@@ -215,71 +215,71 @@ export const Images = () => {
         as well as any other relevant visual or formatting specifications,
         to ensure that images are consistent with the overall look and feel of the brand.</p>
       <div class="flex-container">
-        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff',height: '600px',  overflow: 'scroll' }}>
-          <img src={hairsalon} fluid={true} alt="hair-salon" style={{ paddingTop: '38%'}} />
-          <div className="card-body" style={{height: '600px'}}>
+        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff', height: '600px', overflow: 'scroll' }}>
+          <img src={hairsalon} fluid={true} alt="hair-salon" style={{ paddingTop: '38%' }} />
+          <div className="card-body" style={{ height: '600px' }}>
             <p className="card-title"><h4>Aspect ratio</h4>The aspect ratio of an image refers to the proportional relationship between its width and height.</p>
             <p className="card-text"> {
             /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
               <div class="expandHideCode">react.js
-              <br></br><br></br>
+                <br></br><br></br>
               </div>
               {isExpanded && (
                 <pre>
                   <code>
-                  {`import hairsalon from "../images/hairsalon.jpg";
+                    {`import hairsalon from "../images/hairsalon.jpg";
 <img src={hairsalon} fluid={true} alt="hair-salon" />`}
-                   </code>
+                  </code>
                 </pre>
               )}</p>
-              <p className="card-text"> {
-            /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
-              <div class="expandHideCode">css
-              <br></br><br></br>
-              </div>
-              {isExpanded && (
-                <pre>
-                <code>
-        {`img {
-          width: 100%;
-          height: 60%;
-        }`}
-      </code>
-                </pre>
-              )}</p>
-          </div>
-        </div>
-        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff', height: '600px',  overflow: 'scroll'}}>
-          <img src={hairsalon} class="imgborderCss" fluid={true} alt="hair-salon" style={{marginTop:'45%'}} />
-          <div className="card-body"  style={{height: '600px'}}>
-          <p className="card-title"><h4>Image borders and border radius</h4>An image border is a frame or line that surrounds an image. The border radius property in CSS allows you to specify the rounding of the corners of an element's border.</p>
             <p className="card-text"> {
-            /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
-              <div class="expandHideCode">react.js
-              <br></br><br></br>
-              </div>
-              {isExpanded && (
-                <pre>
-                  <code>
-                  {`import hairsalon from "../images/hairsalon.jpg";
-<img src={hairsalon}  fluid={true} alt="hair-salon" />`}
-                   </code>
-                </pre>
-              )}</p>
-              <p className="card-text"> {
             /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
               <div class="expandHideCode">css
                 <br></br><br></br>
               </div>
               {isExpanded && (
                 <pre>
-                <code>
-        {`img {
+                  <code>
+                    {`img {
+          width: 100%;
+          height: 60%;
+        }`}
+                  </code>
+                </pre>
+              )}</p>
+          </div>
+        </div>
+        <div className="card" style={{ backgroundColor: '#000000', color: '#ffffff', height: '600px', overflow: 'scroll' }}>
+          <img src={hairsalon} class="imgborderCss" fluid={true} alt="hair-salon" style={{ marginTop: '45%' }} />
+          <div className="card-body" style={{ height: '600px' }}>
+            <p className="card-title"><h4>Image borders and border radius</h4>An image border is a frame or line that surrounds an image. The border radius property in CSS allows you to specify the rounding of the corners of an element's border.</p>
+            <p className="card-text"> {
+            /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
+              <div class="expandHideCode">react.js
+                <br></br><br></br>
+              </div>
+              {isExpanded && (
+                <pre>
+                  <code>
+                    {`import hairsalon from "../images/hairsalon.jpg";
+<img src={hairsalon}  fluid={true} alt="hair-salon" />`}
+                  </code>
+                </pre>
+              )}</p>
+            <p className="card-text"> {
+            /*https://blog.logrocket.com/create-collapsible-react-components-react-collapsed/ */}
+              <div class="expandHideCode">css
+                <br></br><br></br>
+              </div>
+              {isExpanded && (
+                <pre>
+                  <code>
+                    {`img {
   border: 1px solid #ddd;
   border-radius: 4px;
   padding: 5px;
 }`}
-      </code>
+                  </code>
                 </pre>
               )}</p>
           </div>
